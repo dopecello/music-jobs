739a2fdb4311e30331f72872781a88ad578bc6b0
Brian Geertsma <briange@tuta.io>
2023-01-28 12:41:37 -0500

    updating buttons to include icons, need to fix spacing

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { AiFillFacebook, AiFillApple } from 'react-icons/ai';
+import { FaGoogle } from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function SignUp() {
   const [email, setEmail] = useState('');
@@ -15,7 +18,7 @@ export default function SignUp() {
       <div className="flex justify-center">
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <h1 className="text-center">Log In</h1>
+            <h1 className="text-center text-4xl">Log In</h1>
           </div>
           <div className="mb-4">
             <input
@@ -37,33 +40,39 @@ export default function SignUp() {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <div className="mb-4">
-            <input type="checkbox" />
-            <label className="pl-2">Keep me signed in</label>
+          <div className="mb-4 flex justify-between space-x-10">
+            <div>
+              <input type="checkbox" />
+              <label className="pl-2">Keep me signed in</label>
+            </div>
+            <div>
+              <Link href={''}>Forgot Password?</Link>
+            </div>
           </div>
           <button
             className="mb-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded w-full"
             type="submit">
             Log In
           </button>
-          <div className="text-center border-t-2 border-gray-500 w-full">
-            or
+          <div className="text-center border-t-2 border-gray-500 w-full p-2"></div>
+          <div className="mb-4">
+            <button className="bg-blue-500 text-white font-bold py-2 px-4 w-full rounded inline-flex items-center">
+              <AiFillFacebook />
+              <span>Log in with Facebook</span>
+            </button>
           </div>
-          <button
-            className="mb-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded w-full"
-            type="submit">
-            Log in with Facebook
-          </button>
-          <button
-            className="mb-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded w-full"
-            type="submit">
-            Log in with Google
-          </button>
-          <button
-            className="mb-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded w-full"
-            type="submit">
-            Log in with Apple
-          </button>
+          <div className="mb-4">
+            <button className="bg-blue-500 text-white font-bold py-2 px-4 w-full rounded inline-flex items-center">
+              <FaGoogle />
+              <span>Log in with Google</span>
+            </button>
+          </div>
+          <div className="mb-4">
+            <button className="bg-black text-white font-bold py-2 px-4 w-full rounded inline-flex items-center">
+              <AiFillApple />
+              <span>Log in with Apple</span>
+            </button>
+          </div>
         </form>
       </div>
     </div>
