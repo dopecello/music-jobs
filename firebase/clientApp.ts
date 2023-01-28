112ebaa67e124f5624fd09ad8059480dfd512842
Brian Geertsma <briange@tuta.io>
2023-01-27 22:54:15 -0500

    updating firebase config to use .env.local variables

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,14 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = initializeApp({
-  apiKey: "AIzaSyBQKBs0gPYEhK8WrMG_hoCQADYlUPEHtRI",
-  authDomain: "musicjobs-99191.firebaseapp.com",
-  projectId: "musicjobs-99191",
-  storageBucket: "musicjobs-99191.appspot.com",
-  messagingSenderId: "737387262953",
-  appId: "1:737387262953:web:a9ba039d88285d4b4b5178",
-  measurementId: "G-G2N40YRJ98",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATA_BASEURL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 });
 
 export const db = getFirestore(firebaseConfig);
